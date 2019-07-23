0d18146c5da4946f4fdbdf6b4369c620ddb3eec3
kine1991 <kine4599@gmail.com>
2019-07-23 10:27:19 +0300

    start working with  fitebase

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React from 'react';
+import { Route, Switch, Link } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.components';
+import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+
+
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Header/>
+      <Switch>
+        <Route exact path="/" component={HomePage}/>
+        <Route path="/shop" component={ShopPage}/>
+        <Route path="/signin" component={SignInAndSignUpPage}/>
+      </Switch>
     </div>
   );
 }
 
 export default App;
+
