e5989cd4eeb3ee92a5e9ad0ad1ef4aecf1670f30
kine1991 <kine4599@gmail.com>
2019-07-26 09:43:22 +0300

    added addItem and removeItem

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,6 @@
+
 import CartActionTypes from './cart.types';
-import { addItemToCart } from './cart.utils'
+import { addItemToCart, removeItemFromCart } from './cart.utils';
 
 const INITIAL_STATE = {
   hidden: true,
@@ -17,8 +18,19 @@ const cartReducer = (state = INITIAL_STATE, action) => {
       return {
         ...state,
         cartItems: addItemToCart(state.cartItems, action.payload)
-        // cartItems: [...state.cartItems, action.payload]
-      }
+      };
+    case CartActionTypes.REMOVE_ITEM:
+      return {
+        ...state,
+        cartItems: removeItemFromCart(state.cartItems, action.payload)
+      };
+    case CartActionTypes.CLEAR_ITEM_FROM_CART:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter(
+          cartItem => cartItem.id !== action.payload.id
+        )
+      };
     default:
       return state;
   }
@@ -26,33 +38,3 @@ const cartReducer = (state = INITIAL_STATE, action) => {
 
 export default cartReducer;
 
-
-
-
-
-// import CartActionTypes from './cart.types';
-// import { addItemToCart } from './cart.utils';
-
-// const INITIAL_STATE = {
-//   hidden: true,
-//   cartItems: []
-// };
-
-// const cartReducer = (state = INITIAL_STATE, action) => {
-//   switch (action.type) {
-//     case CartActionTypes.TOGGLE_CART_HIDDEN:
-//       return {
-//         ...state,
-//         hidden: !state.hidden
-//       };
-//     case CartActionTypes.ADD_ITEM:
-//       return {
-//         ...state,
-//         cartItems: addItemToCart(state.cartItems, action.payload)
-//       };
-//     default:
-//       return state;
-//   }
-// };
-
-// export default cartReducer;
